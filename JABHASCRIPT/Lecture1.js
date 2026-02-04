0dfe7a279b5468801c1a6f9557b1ac6ef7e79cc6
Ritwik-Bug <ritwik450@gmail.com>
2026-02-04 23:44:19 +0530

    Javascript

--- JABHASCRIPT/Lecture1.js
+++ JABHASCRIPT/Lecture1.js
@@ -5,4 +5,52 @@
 
 
 let age1=21;
+
+age1=40;
+age1=null;
+// we can change the value of let variable but we cannot re declare it.
+
 console.log(age1);
+
+const a=21;
+console.log(a);
+// we cannot change the value of const variable and also we cannot re declare it.
+// a=30; // it will give error
+// console.log(a);
+
+
+// primitve data types
+// immutable data types
+// number
+let marks = 85;
+console.log(marks);      
+console.log(typeof marks); 
+
+// string
+let name = "Ritwik";
+console.log(name);        
+console.log(typeof name); 
+
+// boolean
+let isPassed = true;
+console.log(isPassed);        
+console.log(typeof isPassed); 
+
+// non primitive data types
+// mutable data types
+// object
+let student = {
+  name: "Ritwik",
+  age: 20,
+  course: "CSE"
+};
+console.log(student.name); 
+
+// array
+let subjects = ["Math", "Physics", "JavaScript"];
+console.log(subjects[1]); 
+
+// date
+let today = new Date();
+console.log(today);
+console.log(typeof today);

56802308fce0b205cbf307edaab8fffa107a1381
Ritwik-Bug <ritwik450@gmail.com>
2026-02-04 23:30:41 +0530

    JABHASCRIPT

---
+++ JABHASCRIPT/Lecture1.js
@@ -0,0 +1,8 @@
+// we dont use var because it is function scoped,it cannot be used 
+// outside the function but let and const are block scoped they can be used
+// outside the block also.
+// var age=20;
+
+
+let age1=21;
+console.log(age1);
